014e9c53a5a8ad707614aa53951973e38a989303
DinhToang <105490504+DinhToang@users.noreply.github.com>
2023-06-17 13:54:06 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,14 +5,22 @@ import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import Contact from './ContactComponent';
 import { Routes, Route, Navigate } from "react-router-dom";
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -22,35 +30,24 @@ class Main extends Component {
 
   render() {
     const HomePage = () => {
-      return <Home />;
-    };
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
 
     return (
       <div>
         <Header />
         <Routes>
           <Route path="/home" component={HomePage} />
-          <Route
-            exact
-            path="/menu"
-            element={
-              <Menu
-                dishes={this.state.dishes}
-                onClick={(dishId) => this.onDishSelect(dishId)}
-              />
-            }
-          />
-          <Route path="/home" element={<Navigate to="Menu" />} />
+          <Route exact path="/menu" element={<Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>}/>
+          <Route path="/" element={<Navigate to="home" />} />
+          <Route exact path='/contactus' component={Contact}/>
         </Routes>
-
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
-
         <Footer />
       </div>
     );
