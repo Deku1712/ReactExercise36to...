c73384659e178addde4c075914542c3019c20daa
DinhToang <105490504+DinhToang@users.noreply.github.com>
2023-06-17 23:02:28 +0700

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 function RenderDish({ dish }) {
   if (dish == null) {
     return <div></div>;
   }
   return (
-    <div className="col-12 col-md-3">
+    <div className="col-12 col-md-12">
       <Card>
         <CardImg width="100%" src={dish.image} alt={dish.name} />
         <CardBody>
@@ -46,7 +42,7 @@ function RenderComments({ comments }) {
   });
 
   return (
-    <div className="col-12 col-md-5 m-1">
+    <div className="col-12 col-md-12">
       <h4> Comments </h4>
       <ul className="list-unstyled">{showcmnts}</ul>
     </div>
@@ -56,13 +52,29 @@ function RenderComments({ comments }) {
 const DishDetail = (props) => {
   if (props.dish != null) {
     return (
-      <div className="container" key={props.key}>
-        <div className="row">
-          <RenderDish dish={props.dish} />
-          <RenderComments comments={props.dish.comments} />
-        </div>
+      <div className="container">
+      <div className="row">
+          <Breadcrumb>
+
+              <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr />
+          </div>                
       </div>
-    );
+      <div className="row">
+          <div className="col-12 col-md-5 m-1">
+              <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+              <RenderComments comments={props.comments} />
+          </div>
+      </div>
+      </div>
+  );
+
   }
 };
 
