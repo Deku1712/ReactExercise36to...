f253546f2b292644e6a3d4b1f8fae13425ee699b
DinhToang <105490504+DinhToang@users.noreply.github.com>
2023-06-28 15:31:50 +0700

    React Redux Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,29 +5,25 @@ import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
-import Contact from './ContactComponent';
+import Contact from "./ContactComponentV4";
 import { Routes, Route, Navigate, useParams } from "react-router-dom";
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
-import Login from './Login';
-import {withRouter } from 'react-router-dom'
-import { connect } from 'react-redux';
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+import Login from "./Login";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     dishes: state.dishes,
     comments: state.comments,
     promotions: state.promotions,
-    leaders: state.leaders
-  }
-}
-
-
+    leaders: state.leaders,
+  };
+};
 
 class Main extends Component {
-
   constructor(props) {
     super(props);
 
@@ -35,42 +31,56 @@ class Main extends Component {
       dishes: DISHES,
       comments: COMMENTS,
       promotions: PROMOTIONS,
-      leaders: LEADERS
+      leaders: LEADERS,
     };
   }
 
-
   render() {
     const HomePage = () => {
-      return(
-          <Home 
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
-      );
-    }
-
-
-    const DishWithId = () => {
-      const {dishId} = useParams();
-      return(
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(dishId,10))} />
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     };
 
+    const DishWithId = () => {
+      const { dishId } = useParams();
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(dishId, 10)
+          )}
+        />
+      );
+    };
 
     return (
       <div>
         <Header />
         <Routes>
           <Route path="/home" Component={HomePage} />
-          <Route exact path="/menu" element={<Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>}/>
+          <Route
+            exact
+            path="/menu"
+            element={
+              <Menu
+                dishes={this.state.dishes}
+                onClick={(dishId) => this.onDishSelect(dishId)}
+              />
+            }
+          />
           <Route path="/" element={<Navigate to="home" />} />
-          <Route exact path='/login' Component={Login}/>
-          <Route exact path='/contactus' Component={Contact}/>
-          <Route path='/menu/:dishId' Component={DishWithId} />
+          <Route exact path="/login" Component={Login} />
+          <Route exact path="/contactus" Component={Contact} />
+          <Route path="/menu/:dishId" Component={DishWithId} />
         </Routes>
         <Footer />
       </div>
@@ -78,4 +88,4 @@ class Main extends Component {
   }
 }
 
-export default (connect(mapStateToProps)(Main));
+export default connect(mapStateToProps)(Main);
